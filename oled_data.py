f2ae98d5193dadc3bd060d78b141669869617f3e
Salvador P <salvadorperezd@gmail.com>
2022-06-27 15:43:21 +0200

    [CHANGE] Now OLED screen does not flicker.

--- oled_data.py
+++ oled_data.py
@@ -1,71 +1,72 @@
-
-# Optionally adjust the measurement timing budget to change speed and accuracy.
-# See the example here for more details:
-#   https://github.com/pololu/vl53l0x-arduino/blob/master/examples/Single/Single.ino
-# For example a higher speed but less accurate timing budget of 20ms:
-# vl53.measurement_timing_budget = 20000
-# Or a slower but more accurate timing budget of 200ms:
-# vl53.measurement_timing_budget = 200000
-# The default timing budget is 33ms, a good compromise of speed and accuracy.
-
+import math
+import time
+import subprocess
+from PIL import Image, ImageDraw, ImageFont
+from board import SCL, SDA
+import busio
+import adafruit_ssd1306
 import smbus
 import adafruit_vl53l0x
-import time
 import board
 import digitalio
-import busio
-from PIL import Image, ImageDraw, ImageFont
-import adafruit_ssd1306
 
-# Define the Reset Pin
-oled_reset = digitalio.DigitalInOut(board.D4)
-
-# Change these
-# to the right size for your display!
+# Define OLED Sizes.
 WIDTH = 128
-HEIGHT = 64  # Change to 64 if needed
-BORDER = 3
+HEIGHT = 64  # Change to 32 if needed
+
+i2c = busio.I2C(SCL, SDA)
+i2cvl = busio.I2C(board.SCL, board.SDA)
+vl53 = adafruit_vl53l0x.VL53L0X(i2cvl)
+disp = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3C)
+disp.fill(0)
+disp.show()
+i2c = busio.I2C(SCL, SDA)
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+width = disp.width
+height = disp.height
+image = Image.new("1", (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
 # Load default font.
 font = ImageFont.load_default()
 
 bus = smbus.SMBus(1)
+
+# Draw text.
+# draw.text((0, 5), "UCTRONICS", font=font, fill=255)
+
+# Display image.
+disp.image(image)
+disp.show()
+
+# Pause briefly before drawing next frame.
+# time.sleep(3)
+font = ImageFont.load_default()
+
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height - padding
+
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 5
+
 while True:
-
-    # Use for I2C.
-    i2c = board.I2C()
-    i2cvl = busio.I2C(board.SCL, board.SDA)
-    vl53 = adafruit_vl53l0x.VL53L0X(i2cvl)
-    oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3C, reset=oled_reset)
-
-    # Clear display.
-    oled.fill(0)
-    oled.show()
-
-    # Create blank image for drawing.
-    # Make sure to create image with mode '1' for 1-bit color.
-    image = Image.new("1", (oled.width, oled.height))
-
-    # Get drawing object to draw on image.
-    draw = ImageDraw.Draw(image)
-
-    # Draw a white background
-    draw.rectangle((0, 0, oled.width, oled.height), outline=255, fill=255)
-
-    # Draw a smaller inner rectangle
-    draw.rectangle(
-        (BORDER, BORDER, oled.width - BORDER - 1, oled.height - BORDER - 1),
-        outline=0,
-        fill=0,
-    )
-
-
+    # Measure temp and humidity
     # SI7021 address, 0x40(64)
     # Read data, 2 bytes, Humidity MSB first
     rh = bus.read_i2c_block_data(0x40, 0xE5, 2)
     #what really happens here is that master sends a 0xE5 command (measure RH, hold master mode) and read 2 bytes back
     #if you read 3 bytes the last one is the CRC!
-    time.sleep(0.1)
+#    time.sleep(0.1)
     # Convert the data
     humidity = ((rh[0] * 256 + rh[1]) * 125 / 65536.0) - 6
 
@@ -76,49 +77,42 @@ while True:
     #what really happens here is that master sends a 0xE3 command (measure temperature, hold master mode) and read 2 bytes back
     #if you read 3 bytes the last one is the CRC!
 
-    time.sleep(0.1)
+    # time.sleep(0.05)
 
     # Convert the data
-    cTemp = ((temp[0] * 256 + temp[1]) * 175.72 / 65536.0) - 46.85
+    temperature = ((temp[0] * 256 + temp[1]) * 175.72 / 65536.0) - 46.85
 
     # Convert to str the float.
-    cTemp = format(cTemp, '.2f') + " C"
-    humidity = format(humidity, '.2f') + " %"
-    
+    temperature = "Temp: " + format(temperature, '.2f') + " C"
+    humidity = "Humd: " + format(humidity, '.2f') + " %"
+
     # Measure distance
     vl53.measurement_timing_budget = 200000
-    distance = format(vl53.range, '.2f') + " mm"
+    distance = "Dist: " + format(vl53.range, '.2f') + " mm"
 
-    # Draw Some Text
-    text = cTemp
-    (font_width, font_height) = font.getsize(text)
-    draw.text(
-        (oled.width // 2 - font_width // 2, 15),
-        cTemp,
-        font=font,
-        fill=255,
-    )
-    draw.text(
-        (oled.width // 2 - font_width // 2, 25),
-        humidity,
-        font=font,
-        fill=255,
-    )
-    draw.text(
-        (oled.width // 2 - font_width // 2, 35),
-        distance,
-        font=font,
-        fill=255,
-    )
 
-    # Display image
-    oled.image(image)
-    oled.show()
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    cmd1 = "hostname"
+    cmd2 = "hostname -I | cut -d' ' -f1"
+    hostname = subprocess.check_output(cmd1, shell=True).decode("utf-8")
+    ip = subprocess.check_output(cmd2, shell=True).decode("utf-8")
+    cmd = "top -bn1 | grep load | awk '{printf \"Load: %.2f\", $(NF-2)}'"
+    cpu_load = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = "free -m | awk 'NR==2{printf \"Mem: %.0f%%\", $3*100/$2}'"
+    sys_mem = subprocess.check_output(cmd, shell=True).decode("utf-8")
 
-    # print ("Humidity %%RH: %.2f%%" %humidity)
-    print("Humidity: " + humidity)
-    print("Temperature: " + cTemp)
-    print("Distance: " + distance)
-    # print ("Temperature: %.2f°C" %cTemp)
 
-    time.sleep(1)
+    # Write four lines of text.
+    draw.text((x, top + 5),  hostname, font=font, fill=255)
+    draw.text((x + 50, top + 5),  ip, font=font, fill=255)
+    draw.text((x, top + 15), cpu_load, font=font, fill=255)
+    draw.text((x + 70, top + 15), sys_mem, font=font, fill=255)
+    draw.text((x, top + 30),  temperature, font=font, fill=255)
+    draw.text((x, top + 42), humidity, font=font, fill=255)
+    draw.text((x, top + 53), distance, font=font, fill=255)
+
+    # Display image.
+    disp.image(image)
+    disp.show()
+    time.sleep(0.300)
